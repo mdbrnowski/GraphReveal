1be010d935656eb31c8028335aa622980e718d06
Michał Dobranowski <mdbrnowski@gmail.com>
2024-06-30 17:12:08 +0200

    Add query results testing

---
+++ tests/test_query_results.py
@@ -0,0 +1,18 @@
+import pytest
+import sqlite3
+from ..translator.translate import translate
+
+
+@pytest.mark.parametrize('query,expected_count', [
+    ('!bipartite, acyclic', 0),
+])
+def test_query_results(query, expected_count):
+    sql_query = translate(query)
+
+    con = sqlite3.connect("graphs.db")
+    cur = con.cursor()
+
+    print(sql_query)
+
+    result = cur.execute(sql_query)
+    assert len(result.fetchall()) == expected_count
