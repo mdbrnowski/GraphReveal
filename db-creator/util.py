f8cbf7c17094cd67d8e0d61c9f399b6f551b9959
Michał Dobranowski <mdbrnowski@gmail.com>
2024-07-07 18:57:36 +0200

    Implement Bondy-Chvátal Theorem
    resolves #8

--- db-creator/util.py
+++ db-creator/util.py
@@ -1,7 +1,31 @@
 import networkx as nx
 
 
-def is_hamiltonian(G: nx.Graph):
+def _find_closure(G: nx.Graph) -> nx.Graph:
+    """ Returns a graph cl(G) defined as in Bondy-Chvátal Theorem. """
+    G = G.copy()
+    n = G.number_of_nodes()
+    edges_to_add = []
+    for v in G.nodes:
+        for u in G.nodes - G.neighbors(v) - {v}:
+            if G.degree[v] + G.degree[u] >= n:
+                edges_to_add.append((v, u))
+
+    while edges_to_add:
+        v, u = edges_to_add.pop()
+        G.add_edge(v, u)
+        for w in G.nodes - G.neighbors(v) - {v}:
+            if G.degree[w] + G.degree[v] >= n:
+                edges_to_add.append((w, v))
+        for w in G.nodes - G.neighbors(u) - {u}:
+            if G.degree[w] + G.degree[u] >= n:
+                edges_to_add.append((w, u))
+
+    return G
+
+
+def is_hamiltonian(G: nx.Graph) -> bool:
+    """ Checks whether a graph G is hamilton or not. """
     n = G.number_of_nodes()
     if n == 1:
         return True
@@ -12,12 +36,8 @@ def is_hamiltonian(G: nx.Graph):
     if min(d for _, d in G.degree()) == 1:
         return False
 
-    ore_assumption = True
-    for v in G.nodes:
-        for u in G.nodes - G.neighbors(v) - {v}:
-            if G.degree[v] + G.degree[u] < n:
-                ore_assumption = False
-    if ore_assumption:
+    cl_G = _find_closure(G)
+    if cl_G.number_of_edges() == cl_G.number_of_nodes() * (cl_G.number_of_nodes() - 1) / 2:
         return True
 
     return nx.isomorphism.GraphMatcher(G, nx.cycle_graph(n)).subgraph_is_monomorphic()
