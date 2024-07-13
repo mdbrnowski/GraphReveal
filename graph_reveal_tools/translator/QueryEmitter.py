05d1094e529281458b69d0b57e79f1b0a222a09b
Michał Dobranowski <mdbrnowski@gmail.com>
2024-07-13 11:54:09 +0200

    Get rid of `WHERE TRUE AND ...`

--- graph_reveal_tools/translator/QueryEmitter.py
+++ graph_reveal_tools/translator/QueryEmitter.py
@@ -3,46 +3,48 @@ from graph_reveal_tools.translator import QueryParser, QueryParserListener
 
 
 class QueryEmitter(QueryParserListener):
-    result = ""
+    def __init__(self):
+        self.conditions: list[str] = []
 
     def get_result(self):
-        return self.result
+        return "SELECT * FROM graphs WHERE " + " AND ".join(self.conditions)
 
     def enterQuery(self, ctx: QueryParser.QueryContext):
-        self.result += "SELECT * FROM graphs WHERE TRUE"
+        self.conditions.clear()
 
     def enterExpr(self, ctx: QueryParser.ExprContext):
-        self.result += " AND "
         if isinstance(ctx.children[0], TerminalNodeImpl) and ctx.children[0].symbol.type == QueryParser.NOT:
-            self.result += "NOT "
+            self.conditions.append("NOT ")
+        else:
+            self.conditions.append("")
 
     def enterBoolProperty(self, ctx: QueryParser.BoolPropertyContext):
         child = ctx.children[0]
         if child.symbol.type == QueryParser.ACYCLIC:
-            self.result += "acyclic = TRUE"
+            self.conditions[-1] += "acyclic = TRUE"
         if child.symbol.type == QueryParser.BIPARTITE:
-            self.result += "bipartite = TRUE"
+            self.conditions[-1] += "bipartite = TRUE"
         if child.symbol.type == QueryParser.COMPLETE:
-            self.result += "vertices * (vertices - 1) / 2 = edges"
+            self.conditions[-1] += "vertices * (vertices - 1) / 2 = edges"
         if child.symbol.type == QueryParser.CONNECTED:
-            self.result += "components = 1"
+            self.conditions[-1] += "components = 1"
         if child.symbol.type == QueryParser.EULERIAN:
-            self.result += "eulerian = TRUE"
+            self.conditions[-1] += "eulerian = TRUE"
         if child.symbol.type == QueryParser.HAMILTONIAN:
-            self.result += "hamiltonian = TRUE"
+            self.conditions[-1] += "hamiltonian = TRUE"
         if child.symbol.type == QueryParser.NO_ISOLATED_V:
-            self.result += "degree_min > 0"
+            self.conditions[-1] += "degree_min > 0"
         if child.symbol.type == QueryParser.PLANAR:
-            self.result += "planar = TRUE"
+            self.conditions[-1] += "planar = TRUE"
         if child.symbol.type == QueryParser.TREE:
-            self.result += "acyclic = TRUE AND components = 1"
+            self.conditions[-1] += "acyclic = TRUE AND components = 1"
 
     def enterNumEntityExpr(self, ctx: QueryParser.NumEntityExprContext):
         num = ctx.children[0].symbol.text
         entity = ctx.children[1].children[0]
         if entity.symbol.type == QueryParser.VERTEX:
-            self.result += f"vertices = {num}"
+            self.conditions[-1] += f"vertices = {num}"
         if entity.symbol.type == QueryParser.EDGE:
-            self.result += f"edges = {num}"
+            self.conditions[-1] += f"edges = {num}"
         if entity.symbol.type == QueryParser.COMPONENT:
-            self.result += f"components = {num}"
+            self.conditions[-1] += f"components = {num}"
