2400148a051ab3964c58ee5c87e0535b5806164c
Michał Dobranowski <mdbrnowski@gmail.com>
2024-07-20 12:09:08 +0200

    Handle database error
    ref #15

--- graph_reveal_tools/cli.py
+++ graph_reveal_tools/cli.py
@@ -1,7 +1,11 @@
+from sqlite3 import OperationalError
+
+import rich
 import typer
+
 from graph_reveal_tools import get_ids
-from graph_reveal_tools.translator import translate
 from graph_reveal_tools.db_creator import create_db
+from graph_reveal_tools.translator import translate
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -14,10 +18,14 @@ def search(query: str, count: bool = False):
     Currently, graphs with up to 7 vertices are considered.
     """
     sql_query = translate(query)
-    if count:
-        print(len(get_ids(sql_query)))
-    else:
-        print('\n'.join(get_ids(sql_query)))
+    try:
+        if count:
+            print(len(get_ids(sql_query)))
+        else:
+            print('\n'.join(get_ids(sql_query)))
+    except OperationalError as e:
+        rich.print('[bold red]Error:', str(e) + '.')
+        rich.print('Try to create the database first. Run `[cyan]graph-reveal create-database[/cyan]`.')
 
 
 @app.command()
